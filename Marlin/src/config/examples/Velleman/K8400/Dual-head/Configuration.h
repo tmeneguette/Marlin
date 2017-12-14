5eb9d7892d554abcfc5a3f9660488373af88a3a4
Dave Johnson <davejohnson3000@gmail.com>
2017-12-13 21:28:27 -0800

    Fix BED_SIZE configuration

--- Marlin/src/config/examples/Velleman/K8400/Dual-head/Configuration.h
+++ Marlin/src/config/examples/Velleman/K8400/Dual-head/Configuration.h
@@ -788,7 +788,7 @@
 #define Y_MIN_POS 20
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define Y_MAX_POS ((Y_BED_SIZE) + (Y_MIN_POS))
 #define Z_MAX_POS 190
 
 /**
